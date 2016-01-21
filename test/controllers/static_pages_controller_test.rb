2631628173db65558467fef712820e39b34ab8ed
Timothy Levi Campbell <tlc9406@gmail.com>
2016-01-21 18:55:40 +0000

    Use Puma server in prod

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,18 +1,5 @@
 require 'test_helper'
-
 class StaticPagesControllerTest < ActionController::TestCase
-  # Testing root info source: http://www.derekhammer.com/2011/02/19/root-route-testing-in-rails.html
-  test "should route root to home page" do
-    opts = { :controller => 'static_pages', :action => 'home'}
-    assert_recognizes opts, '/'
-  end
-
-  test "should get home" do
-    get :home
-    assert_response :success
-    assert_select "title", "Recordly"
-  end
-
   test "should get help" do
     get :help
     assert_response :success
